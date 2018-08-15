428d686b72ad53a2e73717aa3cd6d71664a5baa2
Kyle <kderrick1988@gmail.com>
2018-08-14 17:01:35 -0700

    add PaymentWSClient

---
+++ src/main/java/com/bharath/trainings/ws/client/PaymentWSClient.java
@@ -0,0 +1,29 @@
+package com.bharath.trainings.ws.client;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.bharath.ws.soap.PaymentProcessor;
+import com.bharath.ws.soap.PaymentProcessorImplService;
+import com.bharath.ws.soap.PaymentProcessorRequest;
+import com.bharath.ws.soap.PaymentProcessorResponse;
+
+public class PaymentWSClient {
+
+	public static void main(String[] args) {
+		try {
+			PaymentProcessorImplService service = new PaymentProcessorImplService(new URL("host:8080/javafirstwsut/services/paymentProcessor?wsdl"));
+			PaymentProcessor port = service.getPaymentProcessorImplPort();
+			
+			PaymentProcessorResponse response = port.processPayment(new PaymentProcessorRequest());
+			System.out.println(response.isResult());
+			
+			
+		} catch (MalformedURLException e) {
+			
+			e.printStackTrace();
+		}
+
+	}
+
+}
