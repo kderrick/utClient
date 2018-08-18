a9aee75d2ea7530721ba10b8d645e61dc4b9ab0f
Kyle <kderrick1988@gmail.com>
2018-08-18 09:23:44 -0700

    update pom to include apache security dependency and configure out Interceptor in PaymentWSClient

--- src/main/java/com/bharath/trainings/ws/client/PaymentWSClient.java
+++ src/main/java/com/bharath/trainings/ws/client/PaymentWSClient.java
@@ -2,6 +2,12 @@ package com.bharath.trainings.ws.client;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
+
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.endpoint.Endpoint;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.ws.security.wss4j.WSS4JOutInterceptor;
 
 import com.bharath.ws.soap.PaymentProcessor;
 import com.bharath.ws.soap.PaymentProcessorImplService;
@@ -14,6 +20,13 @@ public class PaymentWSClient {
 		try {
 			PaymentProcessorImplService service = new PaymentProcessorImplService(new URL("http://localhost:8080/javafirstws/services/paymentProcessor?wsdl"));
 			PaymentProcessor port = service.getPaymentProcessorImplPort();
+			
+			Client client = ClientProxy.getClient(port);
+			Endpoint endpoint = client.getEndpoint();
+			Map<String, Object> props = null;
+			WSS4JOutInterceptor wssOut = new WSS4JOutInterceptor(props);
+			endpoint.getOutInterceptors().add(wssOut);
+			
 			
 			PaymentProcessorResponse response = port.processPayment(new PaymentProcessorRequest());
 			System.out.println(response.isResult());
