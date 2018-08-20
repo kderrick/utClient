7452fecdb6321f5dfd7dc72a02848f86a7bf4500
Kyle <kderrick1988@gmail.com>
2018-08-20 16:38:06 -0700

    update PaymentWSClient to configure the properties action, user and password

--- src/main/java/com/bharath/trainings/ws/client/PaymentWSClient.java
+++ src/main/java/com/bharath/trainings/ws/client/PaymentWSClient.java
@@ -2,12 +2,15 @@ package com.bharath.trainings.ws.client;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.endpoint.Endpoint;
 import org.apache.cxf.frontend.ClientProxy;
 import org.apache.cxf.ws.security.wss4j.WSS4JOutInterceptor;
+import org.apache.wss4j.dom.WSConstants;
+import org.apache.wss4j.dom.handler.WSHandlerConstants;
 
 import com.bharath.ws.soap.PaymentProcessor;
 import com.bharath.ws.soap.PaymentProcessorImplService;
@@ -23,7 +26,13 @@ public class PaymentWSClient {
 			
 			Client client = ClientProxy.getClient(port);
 			Endpoint endpoint = client.getEndpoint();
-			Map<String, Object> props = null;
+			
+			
+			Map<String, Object> props = new HashMap<String, Object>();
+			props.put(WSHandlerConstants.ACTION, WSHandlerConstants.USERNAME_TOKEN);
+			props.put(WSHandlerConstants.USER,"cxf");
+			props.put(WSHandlerConstants.PASSWORD_TYPE, WSConstants.PASSWORD_TEXT);
+			
 			WSS4JOutInterceptor wssOut = new WSS4JOutInterceptor(props);
 			endpoint.getOutInterceptors().add(wssOut);
 			
